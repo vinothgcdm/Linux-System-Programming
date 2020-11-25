d13932ae0d53ce1467d909a025e0153c6b17f363
Vinoth Kumar <vinothg.cdm@gmail.com>
2020-11-25 11:18:06 +0800

    Update main.c to poll for POLLIN on STDIN
    [11:13]gkqv83@zmy33lxcna11 /vinoth: $ time ./a.out
    5 seconds elapsed.

--- 01-File IO/02-poll/main.c
+++ 01-File IO/02-poll/main.c
@@ -6,37 +6,36 @@
 
 int main(void)
 {
-	struct pollfd fds[2];
-	int ret;
+    struct pollfd fds[2];
+    int ret;
 
-	/* watch stdout for ability to write (almost always trye) */
-	fds[0].fd = STDOUT_FILENO;
-	fds[0].events = POLLOUT;
+    /* watch stdout for ability to write (almost always trye) */
+    fds[1].fd = STDOUT_FILENO;
+    fds[1].events = POLLOUT;
 
-	/* eatch stdin for input */
-	fds[1].fd = STDIN_FILENO;
-	fds[1].events = POLLIN;
+    /* eatch stdin for input */
+    fds[0].fd = STDIN_FILENO;
+    fds[0].events = POLLIN;
 
-	/* All set, block! */
-	//ret = poll(fds, 1, TIMEOUT * 1000);
-	ret = poll(fds, 1, -1);
-	if (ret == -1) {
-		perror("poll");
-		return 1;
-	}
+    /* All set, block! */
+    ret = poll(fds, 1, TIMEOUT * 1000);
+    if (ret == -1) {
+        perror("poll");
+        return 1;
+    }
 
-	if (!ret) {
-		printf("%d seconds elapsed.\n", TIMEOUT);
-		return 0;
-	}
+    if (!ret) {
+        printf("%d seconds elapsed.\n", TIMEOUT);
+        return 0;
+    }
 
-	if (fds[0].revents & POLLIN) {
-		printf("stdin is readable\n");
-	}
+    if (fds[0].revents & POLLIN) {
+        printf("stdin is readable\n");
+    }
 
-	if (fds[1].revents & POLLOUT) {
-		printf("stdout is writable.\n");
-	}
+    if (fds[1].revents & POLLOUT) {
+        printf("stdout is writable.\n");
+    }
 
-	return 0;
+    return 0;
 }
